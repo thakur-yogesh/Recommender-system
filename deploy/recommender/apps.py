40966b135f566ac78b247336f204da597566b83a
thakur-yogesh <yt11111999@gmail.com>
2023-04-07 23:04:59 +0530

    adding prediction code for covid, parkinson and heart

--- deploy/recommender/apps.py
+++ deploy/recommender/apps.py
@@ -5,6 +5,11 @@ import pickle
 class RecommenderConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'recommender'
-    path = 'recommender\model\diab_model_final.sav'
-    predictor = pickle.load(open(path, 'rb'))
-    
+    path1 = 'recommender\model\diab_model_final.sav'
+    diab_predictor = pickle.load(open(path1, 'rb'))
+    path2 = 'recommender\model\heart_model_final.sav'
+    heart_predictor = pickle.load(open(path2,'rb'))
+    path3 = 'recommender\model\parkin_model_final.sav'
+    parkin_predictor = pickle.load(open(path3,'rb'))
+    path4 = 'recommender\model\covid_model_final.sav'
+    covid_predictor = pickle.load(open(path4 , 'rb'))
--- deploy/recommender/views.py
+++ deploy/recommender/views.py
@@ -12,70 +12,111 @@ import numpy as np
 import pickle
 import pandas as pd
 import random
-""""
-def diab_pred(person):
-    df = pd.read_csv('recommender\model\diab_db.csv')
-    row = 0
-    for i in df['PERSON']:
-        if i == person:
-            break
-        row = row + 1
-    inputs = [i for i in df.iloc[row][:len(df.iloc[row]) - 1]]
-    return inputs
 
-"""
 class call_model(APIView):
 
     def diab_pred(self ,person, df):
+        in_db = 0
         row = 0
         for i in df['PERSON']:
             if i == person:
+                in_db = 1
                 break
             row = row + 1
-        inputs = [i for i in df.iloc[row][1:len(df.iloc[row]) - 1]]
-        inputs = np.asarray(inputs)
-        inputs = inputs.reshape(1,-1)
-        response = RecommenderConfig.predictor.predict(inputs)
-
-        return response
+        if in_db == 1:
+            inputs = [i for i in df.iloc[row][1:len(df.iloc[row]) - 1]]
+            inputs = np.asarray(inputs)
+            inputs = inputs.reshape(1,-1)
+            response = RecommenderConfig.diab_predictor.predict(inputs)
+            return response
+        else:
+            return [0]
     
     def heart_pred(self , person , df):
-        pass
+        in_db = 0
+        row = 0
+        for i in df['PERSON']:
+            if i == person:
+                in_db = 1
+                break
+            row += 1
+        if in_db == 1:
+            inputs = [i for i in df.iloc[row][1:len(df.iloc[row]) - 1]]
+            inputs = np.asarray(inputs)
+            inputs = inputs.reshape(1,-1)
+            response = RecommenderConfig.heart_predictor.predict(inputs)
+            return response
+        else:
+            return [0]
+        
+
     
     def parkin_pred(self , person , df):
-        pass
+        in_db = 0
+        row = 0
+        for i in df['PERSON']:
+            if i == person:
+                in_db = 1
+                break
+            row += 1
+        if in_db == 1:
+            inputs = [i for i in df.iloc[row][1:len(df.iloc[row]) - 1]]
+            inputs = np.asarray(inputs)
+            inputs = inputs.reshape(1,-1)
+            response = RecommenderConfig.parkin_predictor.predict(inputs)
+            return response
+        else:
+            return [0]
+    
+    def covid_pred(self , person , df):
+        in_db = 0
+        row = 0
+        for i in df['PERSON']:
+            if i == person:
+                in_db = 1
+                break
+            row += 1
+        if in_db == 1: 
+            inputs = [i for i in df.iloc[row][2:len(df.iloc[row])]]
+            inputs = np.asarray(inputs)
+            inputs = inputs.reshape(1,-1)
+            response = RecommenderConfig.covid_predictor.predict(inputs)
+            print("response == ",response)
+            return response
+        else:
+            return [0]
 
 
 
     def get(self , request):
         if request.method == 'GET':
-            params = request.GET.get('sentence')
-            print(params)
-            df = pd.read_csv('recommender\model\diab_db.csv')
-            person = str(df['PERSON'][random.randint(0,len(df))])
-            prediction = self.diab_pred(person , df)
-        
+            #loading csv's
+            df_diab = pd.read_csv('recommender\model\diab_db.csv')
+            df_heart = pd.read_csv('recommender\model\heart_db.csv')
+            df_parkin = pd.read_csv('recommender\model\parkin_db.csv')
+            df_covid = pd.read_csv('recommender\model\covid_db.csv')
 
-            res_dict = {'prediction':str(prediction[0]),
-                        'person':person}  
+
+            #getting a random user from one of the database
+            person = str(df_diab['PERSON'][random.randint(0,len(df_diab))])
+            #params = request.GET.get('donotmatter') #just to check whehter we're getting the request or not
+
+            #print(params)
+            #making predictionns
+            diab_prediction = self.diab_pred(person , df_diab)
+            heart_prediction = self.heart_pred(person,df_heart)
+            parki_prediction = self.parkin_pred(person,df_parkin)
+            covid_predictionn = self.covid_pred(person,df_covid)
+
+
+            #adding the predictions to the dictionary
+            res_dict = {'person':person , 
+                        'diab_prediction':str(diab_prediction[0]),
+                        'heart_prediction':str(heart_prediction[0]),
+                         'parkinson_prediction':str(parki_prediction[0]),
+                          'covid_prediction' :str(covid_predictionn[0]) }  
+            
 
             y = json.dumps(res_dict)
-            return JsonResponse(y , safe=False)
-"""
-def index(request):
-    if request.method == 'GET':
-         path = 'recommender\model\covid_model.sav'
-         predictor = pickle.load(open(path, 'rb'))
-         input_data = [0,11.35,34.56]
-         input_data_as_numpy_array = np.asarray(input_data)
-         input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
-         prediction = predictor.predict(input_data_reshaped)
-
-         my_dict = {"insert_me": str(prediction)}
-    else:
-         my_dict = {"insert_me": "got nothing"}
-    
-
-    return render(request,'index.html',context=my_dict)
-
-"""
+            print(y)
+            return JsonResponse(res_dict , safe=False)
